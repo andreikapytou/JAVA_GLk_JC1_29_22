af3cab75f88700ea97e5de0520cbafa62af42a55
”Andrei <andreikapytou@gmail.com>
2022-10-21 16:25:24 +0300

    Задание №2

--- src/JAVA_GLk_JC1_29_22/HomeTasks/Task2/AllTypesOfVariables.java
+++ src/JAVA_GLk_JC1_29_22/HomeTasks/Task2/AllTypesOfVariables.java
@@ -150,6 +150,7 @@ public class AllTypesOfVariables {
         this.variableGlobalPrimitiveBoolean = variableGlobalPrimitiveBoolean;
     }
 
+
     public char getPrimitiveChar() {
 
         return variableGlobalPrimitiveChar;
