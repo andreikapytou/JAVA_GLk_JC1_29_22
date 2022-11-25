629c7a8fbea26183038303dd81782b51cfcc9aff
”Andrei <andreikapytou@gmail.com>
2022-11-25 21:33:59 +0300

    Задание №11. Заказы - Товары. Продолжение 1.

---
+++ src/JAVA_GLk_JC1_29_22/HomeTasks/task11/MenuDialog.java
@@ -0,0 +1,162 @@
+package JAVA_GLk_JC1_29_22.HomeTasks.task11;
+
+import java.util.Scanner;
+
+public class MenuDialog {
+
+    private static final String[] PRODUCTS_NAMES1 = {"Очки", "Плавки", "Велосипед", "Нож", "Чашка", "Лампа", "Книга"};
+    private static final String[] PRODUCTS_NAMES2 = {"Вилка", "Ботинки", "Скатерть", "Топор", "Термос", "Лопата", "Спички"};
+
+    private static final String ACTION1 = "1";
+    private static final String ACTION2 = "2";
+    private static final String ACTION3 = "3";
+    private static final String ACTION4 = "4";
+    private static final String ACTION5 = "5";
+    private static final String ACTION6 = "6";
+    private static final String ACTION7 = "7";
+    private static final String ACTION8 = "8";
+    private static final String ACTION9 = "9";
+    private static final String ACTION10 = "10";
+    private static final String ACTION11 = "10";
+    private static final String SUB_ACTION1 = "1";
+    private static final String SUB_ACTION2 = "2";
+
+    private static final String EXIT1 = "q";
+    private static final String EXIT2 = "Q";
+
+    private static final String PROD_WAREHOUSE = "Перечень товаров на СКЛАДЕ:";
+
+    private static final Shop shop = new Shop();
+
+    private MenuDialog() {
+
+    }
+
+    public static void startApplication() {
+
+        shop.unloadProductsInWarehouse(PRODUCTS_NAMES1);
+        shop.fixDateProductsInWarehouse();
+
+        chooseMenuAction();
+    }
+
+    private static void chooseMenuAction() {
+
+        String selectedAction = "";
+        do {
+            selectedAction = printMenuBasicText();
+            makeSelectedBaseAction(selectedAction);
+        }
+        while(!isCheckExitCondition(selectedAction));
+
+        System.out.println("Выход.");
+    }
+
+    private static boolean isCheckExitCondition(String condition){
+
+        return condition.equals(EXIT1) || condition.equals(EXIT2);
+    }
+
+    private static void makeSelectedBaseAction(String action){
+
+        if(action.equals(ACTION1)) {
+            System.out.println("Для вывода на консоль списка товаров доступных для заказ следуйте инструкции.");
+            chooseSubMenuAction();
+        } else if (action.equals(ACTION2)) {
+
+        } else if (action.equals(ACTION3)) {
+
+        } else if (action.equals(ACTION4)) {
+
+        } else if (action.equals(ACTION5)) {
+
+        } else if (action.equals(ACTION6)) {
+
+        } else if (action.equals(ACTION7)) {
+
+        } else if (action.equals(ACTION8)) {
+
+        } else if (action.equals(ACTION9)) {
+
+        } else if (action.equals(ACTION10)) {
+
+        } else if (action.equals(ACTION11)) {
+
+        } else if (isCheckExitCondition(action)) {
+            System.out.println("Выход из программы.");
+        } else {
+            System.out.printf("Введен №:%s - действие с данным номером отсутсвует в меню.\n\n", action);
+        }
+    }
+
+    private static String printMenuBasicText() {
+
+        System.out.println("Выбирете одно из желаемых действий:");
+        System.out.println();
+        System.out.printf(" %s -  Вывести список товаров доступных для заказа.\n", ACTION1);
+        System.out.printf(" %s -  Вывести список товаров в корзине.\n", ACTION2);
+        System.out.printf(" %s -  Вывести список закзов.\n", ACTION3);
+        System.out.printf(" %s -  Добавить - создать новый заказ.\n", ACTION4);
+        System.out.printf(" %s -  Редактировать - существующие заказы.\n", ACTION5);
+        System.out.printf(" %s -  Удалить заказ.\n", ACTION6);
+        System.out.printf(" %s -  Добавить товар к текущему заказу.\n", ACTION7);
+        System.out.printf(" %s -  Удалить товар из текущего заказа.\n", ACTION8);
+        System.out.printf(" %s -  Добавить товар в корзину.\n", ACTION9);
+        System.out.printf(" %s -  Удалить товар из корзины.\n", ACTION10);
+        System.out.printf(" %s -  Добавить все товары из корзины к текущему заказу.\n", ACTION11);
+        System.out.println(" Q -  Выход из программы.");
+        System.out.println();
+        System.out.print("Введите номер желаемого дейсвия и нажмите клавишу Enter : ");
+
+        return inputConsole();
+    }
+
+    private static String printSubMenuBasicText() {
+
+        System.out.println("Выбирете одно из желаемых действий:");
+        System.out.printf("%s - Вывести полную информации.\n", SUB_ACTION1);
+        System.out.printf("%s - Вывести сокращенную информацию.\n", SUB_ACTION2);
+        System.out.println("Q - Вернуться в основное меню.");
+        System.out.print("Введите номер желаемого дейсвия и нажмите клавишу Enter : ");
+
+        return inputConsole();
+    }
+
+    private static void chooseSubMenuAction() {
+
+        String subAction = "";
+        do {
+            subAction = printSubMenuBasicText();
+            makeSelectedSubAction(subAction);
+        }
+        while(!isCheckExitCondition(subAction));
+    }
+
+    private static void makeSelectedSubAction(String subAction){
+
+        if(subAction.equals(SUB_ACTION1)){
+            System.out.println(shop.getStringInfoListProducts(PROD_WAREHOUSE,true, shop.getProductsInWarehouse()));
+        } else if (subAction.equals(SUB_ACTION2)){
+            System.out.println(shop.getStringInfoListProducts(PROD_WAREHOUSE,false, shop.getProductsInWarehouse()));
+        } else if (isCheckExitCondition(subAction)) {
+            System.out.println("Возврат в главное меню.\n");
+        } else {
+            System.out.printf("Введен №:%s - действие с данным номером отсутсвует в меню.\n", subAction);
+        }
+    }
+
+
+    private static String inputConsole(String strWelcomeInput) {
+
+        System.out.print(strWelcomeInput);
+        Scanner inputData = new Scanner(System.in);
+        return inputData.nextLine();
+    }
+
+    private static String inputConsole() {
+
+        Scanner inputData = new Scanner(System.in);
+        return inputData.nextLine();
+    }
+
+}
--- src/JAVA_GLk_JC1_29_22/HomeTasks/task11/Shop.java
+++ src/JAVA_GLk_JC1_29_22/HomeTasks/task11/Shop.java
@@ -4,45 +4,16 @@ import java.util.*;
 
 public class Shop {
 
-    private static final String[] PRODUCTS_NAMES = {"Очки", "Плавки", "Велосипед", "Нож", "Чашка", "Лампа", "Книга"};
-    
     private static List<Product> productsInWarehouse = new ArrayList<>();
     private List<Product> productsInBasket = new ArrayList<>();
     private static List<Order> listOrders = new ArrayList<>();
 
-      /*
-     GregorianCalendar calendar = new GregorianCalendar(2017, Calendar.JANUARY , 25);
-     Calendar calendar = new GregorianCalendar();
-   calendar.set(Calendar.YEAR, 2017);
-   calendar.set(Calendar.MONTH, 0);
-   calendar.set(Calendar.DAY_OF_MONTH, 25);
-   calendar.set(Calendar.HOUR_OF_DAY, 19);
-   calendar.set(Calendar.MINUTE, 42);
-   calendar.set(Calendar.SECOND, 12);
-
-   System.out.println(calendar.getTime());
-
-   System.out.println("Год: " + calendar.get(Calendar.YEAR));
-   System.out.println("Месяц: " + calendar.get(Calendar.MONTH));
-   System.out.println("Порядковый номер недели в месяце: " + calendar.get(Calendar.WEEK_OF_MONTH));//порядковый номер недели в месяце
-
-   System.out.println("Число: " + calendar.get(Calendar.DAY_OF_MONTH));
-
-   System.out.println("Часы: " + calendar.get(Calendar.HOUR));
-   System.out.println("Минуты: " + calendar.get(Calendar.MINUTE));
-   System.out.println("Секунды: " + calendar.get(Calendar.SECOND));
-   System.out.println("Миллисекунды: " + calendar.get(Calendar.MILLISECOND));
-
-   DateFormat df = new SimpleDateFormat("dd MMM yyy GG");
-   System.out.println(df.format(calendar.getTime()));
-
-    */
 
     private void basket() {
 
     }
 
-    private static void unloadProductsInWarehouse(String[] productsNames) {
+    public void unloadProductsInWarehouse(String[] productsNames) {
 
         for(int i=0; i< productsNames.length; i++){
             productsInWarehouse.add(new Product(i+1, productsNames[i]));
@@ -54,34 +25,34 @@ public class Shop {
         productsInWarehouse.add(new Product(123, "Ваза", dataProduct));
      }
 
-    private static void fixDateProductsInWarehouse(){
+    public void fixDateProductsInWarehouse(){
 
          for(int i = 0; i<productsInWarehouse.size()-1; i++){
 
              Product product = productsInWarehouse.get(i);
              Calendar lastDate =  product.getDateOfManufacture();
-             fixNewYearMountDay(lastDate, 2000+i, 11, 1);
+             fixNewYearMountDay(lastDate, 2000+i, 1+i, 1+i);
              fixNewHourMinuteSecond(lastDate, 8+i, 1+i, 20+i);
              product.setDateOfManufacture(lastDate);
              productsInWarehouse.set(i, product);
          }
     }
 
-    private static void fixNewYearMountDay(Calendar calendar, int year, int month, int day){
+    private void fixNewYearMountDay(Calendar calendar, int year, int month, int day){
 
         calendar.set(Calendar.YEAR, year);
         calendar.set(Calendar.MONTH, month);
         calendar.set(Calendar.DAY_OF_MONTH, day);
     }
 
-    private static void fixNewHourMinuteSecond(Calendar calendar, int hour, int minute, int second){
+    private void fixNewHourMinuteSecond(Calendar calendar, int hour, int minute, int second){
 
         calendar.set(Calendar.HOUR_OF_DAY, hour);
         calendar.set(Calendar.MINUTE, minute);
         calendar.set(Calendar.SECOND, second);
     }
 
-    private static String getStringInfoListProducts(String source, boolean fullInfo, List<Product> listProducts){
+    public String getStringInfoListProducts(String source, boolean fullInfo, List<Product> listProducts){
 
         String infoProducts = "";
         if(fullInfo){
@@ -99,7 +70,7 @@ public class Shop {
         return infoProducts;
     }
 
-    private static  String getStringInfoListOrders(String source, boolean fullInfo, List<Order> listOrders){
+    public String getStringInfoListOrders(String source, boolean fullInfo, List<Order> listOrders){
 
         String infoOrders = "";
         if(fullInfo){
@@ -118,15 +89,13 @@ public class Shop {
         return infoOrders;
     }
 
-    public static void startApplication() {
+    public void startApplication() {
 
-        unloadProductsInWarehouse(PRODUCTS_NAMES);
-        fixDateProductsInWarehouse();
         //productsInWarehouse.forEach( nextProduct -> System.out.println(nextProduct.toString()) );
-        System.out.println(getStringInfoListProducts("Перечень товаров на СКЛАДЕ:",false, productsInWarehouse));
 
         Order order = new Order(1);
         order.addProducts(productsInWarehouse);
+        order.addProduct( new Product(666, "Галеты"));
         listOrders.add(order);
         listOrders.add(new Order(2, productsInWarehouse));
         listOrders.add(new Order(3, productsInWarehouse));
@@ -134,10 +103,18 @@ public class Shop {
 
     }
 
-    private static void menuDialog() {
-
+    public static List<Product> getProductsInWarehouse() {
+        return productsInWarehouse;
     }
 
-    private Shop() {
+    public List<Product> getProductsInBasket() {
+        return productsInBasket;
+    }
+
+    public static List<Order> getListOrders() {
+        return listOrders;
+    }
+
+    public Shop() {
     }
 }
